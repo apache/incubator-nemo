9ac7d8b9e45f59858525e3f5d9f633b84412f6b5
John Yang <johnyangk@gmail.com>
2016-12-27 15:42:58 +0900

    Initial Working Code  (#26)
    Initial Working Code

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 # Vortex 
-[![Build Status](http://cmscluster.snu.ac.kr/jenkins/job/Vortex-master/badge/icon)](http://cmscluster.snu.ac.kr/jenkins/job/Vortex-master/)
+[![Build Status](http://cmscluster.snu.ac.kr:8080/jenkins/buildStatus/icon?job=Vortex-master)](http://cmscluster.snu.ac.kr:8080/jenkins/job/Vortex-master/)
 
 Vortex is a general-purpose data-processing engine.
 
+## Components
+* translator: Translates various high-level user programs for the compiler.
+* compiler: Compiles user programs into a physical execution plan.
+* runtime: Physically executes the plan.
 
 ## Requirements
+* Beam 0.4.0-incubating-SNAPSHOT (You must download it from https://github.com/apache/incubator-beam and build it)
 * Java 8
 * Maven
-* REEF
+
+## Examples
+* java -cp target/vortex-0.1-SNAPSHOT-shaded.jar edu.snu.vortex.translator.beam.examples.MapReduce ./src/main/resources/sample_input ./src/main/resources/sample_output
+* java -cp target/vortex-0.1-SNAPSHOT-shaded.jar edu.snu.vortex.translator.beam.examples.Broadcast ./src/main/resources/sample_input ./src/main/resources/sample_output
+* java -cp target/vortex-0.1-SNAPSHOT-shaded.jar edu.snu.vortex.compiler.examples.MapReduce
---
+++ src/main/java/edu/snu/vortex/compiler/plan/Edge.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2016 Seoul National University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.snu.vortex.compiler.plan;
+
+import edu.snu.vortex.compiler.plan.node.Node;
+
+import java.util.HashMap;
+
+/**
+ * Physical execution plan of intermediate data movement.
+ */
+public class Edge<I, O> {
+  public enum Type {
+    M2M,
+    O2M,
+    O2O,
+  }
+
+  private final String id;
+  private final HashMap<Attributes.Key, Attributes.Val> attributes;
+  private final Type type;
+  private final Node<?, I> src;
+  private final Node<O, ?> dst;
+
+  Edge(final Type type,
+       final Node<?, I> src,
+       final Node<O, ?> dst) {
+    this.id = IdManager.newEdgeId();
+    this.attributes = new HashMap<>(0);
+    this.type = type;
+    this.src = src;
+    this.dst = dst;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public Edge<I, O> setAttr(final Attributes.Key key, final Attributes.Val val) {
+    attributes.put(key, val);
+    return this;
+  }
+
+  public Attributes.Val getAttr(final Attributes.Key key) {
+    return attributes.get(key);
+  }
+
+  public Type getType() {
+    return type;
+  }
+
+  public Node<?, I> getSrc() {
+    return src;
+  }
+
+  public Node<O, ?> getDst() {
+    return dst;
+  }
+
+  @Override
+  public String toString() {
+    final StringBuilder sb = new StringBuilder();
+    sb.append("id: ");
+    sb.append(id);
+    sb.append(", src: ");
+    sb.append(src.getId());
+    sb.append(", dst: ");
+    sb.append(dst.getId());
+    sb.append(", attributes: ");
+    sb.append(attributes);
+    sb.append(", type: ");
+    sb.append(type);
+    return sb.toString();
+  }
+}
