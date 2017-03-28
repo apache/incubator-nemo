c9b85d55a4d0b40415f038e35858ee7a439a1fda
gwsshs22 <gwsshs22@users.noreply.github.com>
2017-03-28 16:32:40 +0900

    [Nemo-82] Introduce messaging components (#114)
    Resolves #82 
    This addresses the issue by
     * Introduce interfaces for exchange messages
     * Implement interfaces on runtime.common.message for local environment
     * Add a test case for local messaging components

---
+++ src/main/java/edu/snu/vortex/runtime/common/message/local/LocalMessageEnvironment.java
@@ -0,0 +1,39 @@
+package edu.snu.vortex.runtime.common.message.local;
+
+import edu.snu.vortex.runtime.common.message.*;
+
+import java.io.Serializable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+
+/**
+ * A simple {@link MessageEnvironment} implementation that works on a single node.
+ */
+public final class LocalMessageEnvironment implements MessageEnvironment {
+
+  private final String currentNodeId;
+  private final LocalMessageDispatcher dispatcher;
+
+  public LocalMessageEnvironment(final String currentNodeId, final LocalMessageDispatcher dispatcher) {
+    this.currentNodeId = currentNodeId;
+    this.dispatcher = dispatcher;
+  }
+
+  @Override
+  public <T extends Serializable> MessageSender<T> setupListener(
+      final String messageTypeId, final MessageListener<T> listener) {
+    return dispatcher.setupListener(currentNodeId, messageTypeId, listener);
+  }
+
+  @Override
+  public <T extends Serializable> Future<MessageSender<T>> asyncConnect(
+      final String targetId, final String messageTypeId) {
+    return CompletableFuture.completedFuture(new LocalMessageSender<T>(
+        currentNodeId, targetId, messageTypeId, dispatcher));
+  }
+
+  @Override
+  public String getCurrentId() {
+    return currentNodeId;
+  }
+}
