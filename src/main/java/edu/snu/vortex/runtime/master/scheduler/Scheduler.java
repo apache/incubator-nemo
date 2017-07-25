cb877b2f42e471989a16ea485e6edb08d7e916bd
jooykim <jykim88@gmail.com>
2017-07-25 16:31:24 +0900

    Add missing comments for interfaces (#368)
    This PR adds missing comments for interfaces for clarification.

--- src/main/java/edu/snu/vortex/runtime/master/scheduler/Scheduler.java
+++ src/main/java/edu/snu/vortex/runtime/master/scheduler/Scheduler.java
@@ -27,13 +27,38 @@ import java.util.List;
  */
 @DefaultImplementation(BatchScheduler.class)
 public interface Scheduler {
+
+  /**
+   * Schedules the given job.
+   * @param physicalPlan of the job being submitted.
+   * @param maxScheduleAttempt the max. number of times a stage can be attempted for execution.
+   * @return the {@link JobStateManager} for the submitted job to keep track of the execution states.
+   */
   JobStateManager scheduleJob(final PhysicalPlan physicalPlan,
                               final int maxScheduleAttempt);
 
+  /**
+   * Called when an executor is added to Runtime, so that the extra resource can be used to execute the job.
+   * @param executorId of the executor that has been added.
+   */
   void onExecutorAdded(String executorId);
 
+  /**
+   * Called when an executor is removed from Runtime, so that faults related to the removal can be handled.
+   * @param executorId of the executor that has been removed.
+   */
   void onExecutorRemoved(String executorId);
 
+  /**
+   * Called when a TaskGroup's execution state changes.
+   * @param executorId of the executor in which the TaskGroup is executing.
+   * @param taskGroupId of the TaskGroup whose state must be updated.
+   * @param newState for the TaskGroup.
+   * @param attemptIdx the number of times this TaskGroup has executed.
+   *************** the below parameters are only valid for failures *****************
+   * @param failedTaskIds the IDs of the failed Tasks of the TaskGroup upon failure.
+   * @param failureCause for which the TaskGroup failed in the case of a recoverable failure.
+   */
   void onTaskGroupStateChanged(final String executorId,
                                final String taskGroupId,
                                final TaskGroupState.State newState,
@@ -41,5 +66,9 @@ public interface Scheduler {
                                final List<String> failedTaskIds,
                                final TaskGroupState.RecoverableFailureCause failureCause);
 
+  /**
+   * To be called when a job should be terminated.
+   * Any clean up code should be implemented in this method.
+   */
   void terminate();
 }
