9a4b6874c4f32520240db83fe35c2b32ae64726a
Sanha Lee <sanhaleehana@gmail.com>
2017-07-26 16:23:34 +0900

    fix pado pass (#379)
    This PR temporally resolves the OOM issue during 12GB Pado MR with constrained resources.
    It would be good to consider the stage-level garbage collection / partition GC upon consumption.

--- src/main/java/edu/snu/vortex/compiler/optimizer/passes/PadoEdgePass.java
+++ src/main/java/edu/snu/vortex/compiler/optimizer/passes/PadoEdgePass.java
@@ -33,7 +33,7 @@ public final class PadoEdgePass implements Pass {
       if (!inEdges.isEmpty()) {
         inEdges.forEach(edge -> {
           if (fromTransientToReserved(edge)) {
-            edge.setAttr(Attribute.Key.ChannelDataPlacement, Attribute.Memory);
+            edge.setAttr(Attribute.Key.ChannelDataPlacement, Attribute.LocalFile);
             edge.setAttr(Attribute.Key.ChannelTransferPolicy, Attribute.Push);
           } else if (fromReservedToTransient(edge)) {
             edge.setAttr(Attribute.Key.ChannelDataPlacement, Attribute.LocalFile);
