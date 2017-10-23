f16f320a9d3eea3442b0575271a50af5f64ae902
John Yang <johnyangk@apache.org>
2017-10-23 13:48:13 +0900

    core

--- src/main/java/edu/snu/onyx/runtime/master/resource/ContainerManager.java
+++ src/main/java/edu/snu/onyx/runtime/master/resource/ContainerManager.java
@@ -122,7 +122,7 @@ public final class ContainerManager {
       evaluatorRequestor.submit(EvaluatorRequest.newBuilder()
           .setNumber(numToRequest)
           .setMemory(resourceSpecification.getMemory())
-          // .setNumberOfCores(resourceSpecification.getCapacity())
+          .setNumberOfCores(1)
           .build());
     } else {
       LOG.info("Request {} containers", numToRequest);
