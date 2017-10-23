9d606c3eacddef6f56d685a264cf7e9dad85649f
Joo Yeon Kim <jykim88@gmail.com>
2017-10-23 17:59:31 +0900

    Change pass

--- src/main/java/edu/snu/onyx/compiler/optimizer/pass/compiletime/annotating/ReducerParallelism1for1gbPass.java
+++ src/main/java/edu/snu/onyx/compiler/optimizer/pass/compiletime/annotating/ReducerParallelism1for1gbPass.java
@@ -57,7 +57,7 @@ public final class ReducerParallelism1for1gbPass extends AnnotatingPass {
                   ScatterGather.class.equals(edge.getProperty(ExecutionProperty.Key.DataCommunicationPattern)))
               .findAny();
           if (shuffle.isPresent()) {
-            vertex.setProperty(ParallelismProperty.of(1));
+            vertex.setProperty(ParallelismProperty.of(4));
           } else {
             final OptionalInt parallelism = inEdges.stream()
                 // No reason to propagate via Broadcast edges, as the data streams that will use the broadcasted data
