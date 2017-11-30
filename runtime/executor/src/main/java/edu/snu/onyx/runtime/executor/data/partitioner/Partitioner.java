a2de9719d9581b9f6da78072e65dcc037036221c
Sanha Lee <sanhaleehana@gmail.com>
2017-11-30 15:47:54 +0900

    move partitioner (#619)

--- runtime/executor/src/main/java/edu/snu/onyx/runtime/executor/data/partitioner/Partitioner.java
+++ runtime/executor/src/main/java/edu/snu/onyx/runtime/executor/data/partitioner/Partitioner.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package edu.snu.onyx.runtime.common.partitioner;
+package edu.snu.onyx.runtime.executor.data.partitioner;
 
 import edu.snu.onyx.common.KeyExtractor;
 import edu.snu.onyx.runtime.common.data.Block;
