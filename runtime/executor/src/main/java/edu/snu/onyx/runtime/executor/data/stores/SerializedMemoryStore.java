990e6a6dcc428cafe6fde07461ae10664c81452a
Sanha Lee <sanhaleehana@gmail.com>
2017-12-05 12:15:17 +0900

    [Nemo-606] Support put / get serialized block in each PartitionStore (#625)
    This PR:
    - Enables `Block` to contain either serialized or non-serialized data (but not contain both).
    - Enables `DataUtil` to convert between serialized and non-serialized `Block`.
    - Makes all `Partition`s and `PartitionStore`s to support write & read **serialized** block formally.
    
    Resolves #606.

--- runtime/executor/src/main/java/edu/snu/onyx/runtime/executor/data/stores/SerializedMemoryStore.java
+++ runtime/executor/src/main/java/edu/snu/onyx/runtime/executor/data/stores/SerializedMemoryStore.java
@@ -16,60 +16,28 @@
 package edu.snu.onyx.runtime.executor.data.stores;
 
 import edu.snu.onyx.common.coder.Coder;
-import edu.snu.onyx.common.exception.PartitionFetchException;
-import edu.snu.onyx.runtime.executor.data.DataUtil;
-import edu.snu.onyx.runtime.common.data.HashRange;
 import edu.snu.onyx.runtime.executor.data.PartitionManagerWorker;
 import edu.snu.onyx.runtime.executor.data.partition.SerializedMemoryPartition;
 import org.apache.reef.tang.InjectionFuture;
 
 import javax.annotation.concurrent.ThreadSafe;
 import javax.inject.Inject;
-import java.io.IOException;
-import java.util.Optional;
 
 /**
  * Serialize and store data in local memory.
  */
 @ThreadSafe
 public final class SerializedMemoryStore extends LocalPartitionStore {
-  public static final String SIMPLE_NAME = "SerializedMemoryStore";
-  private final InjectionFuture<PartitionManagerWorker> partitionManagerWorker;
 
   @Inject
   private SerializedMemoryStore(final InjectionFuture<PartitionManagerWorker> partitionManagerWorker) {
-    super();
-    this.partitionManagerWorker = partitionManagerWorker;
+    super(partitionManagerWorker);
   }
 
   @Override
   public void createPartition(final String partitionId) {
-    final Coder coder = DataUtil.getCoderFromWorker(partitionId, partitionManagerWorker.get());
+    final Coder coder = getCoderFromWorker(partitionId);
     getPartitionMap().put(partitionId, new SerializedMemoryPartition(coder));
-  }
-
-  /**
-   * Retrieves serialized data (array of bytes) in a specific {@link HashRange} from a partition.
-   *
-   * @param partitionId of the target partition.
-   * @param hashRange   the hash range.
-   * @return the result data from the target partition (if the target partition exists).
-   * @throws PartitionFetchException if fail to get data.
-   */
-  public Optional<Iterable<byte[]>> getSerializedBlocksFromPartition(final String partitionId,
-                                                                     final HashRange hashRange)
-      throws PartitionFetchException {
-    final SerializedMemoryPartition partition = (SerializedMemoryPartition) getPartitionMap().get(partitionId);
-
-    if (partition != null) {
-      try {
-        return Optional.of(partition.retrieveSerializedElements(hashRange));
-      } catch (final IOException e) {
-        throw new PartitionFetchException(e);
-      }
-    } else {
-      return Optional.empty();
-    }
   }
 
   /**
