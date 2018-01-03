765992a808f824fd26e4310b79be6a13317360ac
sanha <sanhaleehana@naver.com>
2018-01-03 12:58:15 +0900

    more log

--- runtime/executor/src/main/java/edu/snu/onyx/runtime/executor/datatransfer/InputReader.java
+++ runtime/executor/src/main/java/edu/snu/onyx/runtime/executor/datatransfer/InputReader.java
@@ -28,6 +28,8 @@ import edu.snu.onyx.common.exception.BlockFetchException;
 import edu.snu.onyx.common.exception.UnsupportedCommPatternException;
 import edu.snu.onyx.runtime.common.data.HashRange;
 import edu.snu.onyx.runtime.executor.data.BlockManagerWorker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
 import java.util.ArrayList;
@@ -44,6 +46,7 @@ import java.util.stream.StreamSupport;
  * TODO #492: Modularize the data communication pattern.
  */
 public final class InputReader extends DataTransfer {
+  private static final Logger LOG = LoggerFactory.getLogger(InputReader.class.getName());
   private final int dstTaskIndex;
   private final String taskGroupId;
   private final BlockManagerWorker blockManagerWorker;
@@ -131,10 +134,13 @@ public final class InputReader extends DataTransfer {
     final List<CompletableFuture<Iterable>> futures = new ArrayList<>();
     for (int srcTaskIdx = 0; srcTaskIdx < numSrcTasks; srcTaskIdx++) {
       final String blockId = RuntimeIdGenerator.generateBlockId(getId(), srcTaskIdx);
-      futures.add(
-          blockManagerWorker.retrieveDataFromBlock(blockId, getId(),
-              (DataStoreProperty.Value) runtimeEdge.getProperty(ExecutionProperty.Key.DataStore),
-              hashRangeToRead));
+      final CompletableFuture<Iterable> future = blockManagerWorker.retrieveDataFromBlock(blockId, getId(),
+          (DataStoreProperty.Value) runtimeEdge.getProperty(ExecutionProperty.Key.DataStore),
+          hashRangeToRead);
+      future.whenComplete((itr, exp) -> {
+        LOG.info("Resolved read future for " + blockId);
+      });
+      futures.add(future);
     }
 
     return futures;
