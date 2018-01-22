4ad8811ea9c068647876c1a6a8cde0f3abec5f8c
skystar-p <usezmap@gmail.com>
2018-01-22 12:38:18 +0900

    Implement CompressionFilter

---
+++ runtime/executor/src/main/java/edu/snu/onyx/runtime/executor/data/filter/CompressionFilter.java
@@ -0,0 +1,40 @@
+package edu.snu.onyx.runtime.executor.data.filter;
+
+import edu.snu.onyx.common.exception.UnsupportedCompressionException;
+import edu.snu.onyx.common.ir.edge.executionproperty.CompressionProperty;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+public class CompressionFilter implements Filter {
+  private final CompressionProperty.Compression compression;
+
+  public CompressionFilter(CompressionProperty.Compression compression) {
+    this.compression = compression;
+  }
+
+  @Override
+  public InputStream wrapInput(final InputStream in) throws IOException {
+    switch (compression) {
+      case Gzip:
+        return new GZIPInputStream(in);
+      case LZ4:
+      default:
+        throw new UnsupportedCompressionException("Not supported compression method");
+    }
+  }
+
+  @Override
+  public OutputStream wrapOutput(final OutputStream out) throws IOException {
+    switch (compression) {
+      case Gzip:
+        return new GZIPOutputStream(out);
+      case LZ4:
+      default:
+        throw new UnsupportedCompressionException("Not supported compression method");
+    }
+  }
+}
