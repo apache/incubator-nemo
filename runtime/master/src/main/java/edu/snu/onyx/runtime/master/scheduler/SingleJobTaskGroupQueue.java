692ae769dd865ca29a9792f95984207809e1e403
sanha <sanhaleehana@naver.com>
2018-02-01 15:54:42 +0900

    debug

--- runtime/master/src/main/java/edu/snu/onyx/runtime/master/scheduler/SingleJobTaskGroupQueue.java
+++ runtime/master/src/main/java/edu/snu/onyx/runtime/master/scheduler/SingleJobTaskGroupQueue.java
@@ -124,8 +124,8 @@ public final class SingleJobTaskGroupQueue implements PendingTaskGroupQueue {
           stageIdToPendingTaskGroups.forEach((scheduledStageId, taskGroupList) ->
               updateSchedulableStages(scheduledStageId, taskGroupList.getFirst().getContainerType()));
         } else {
-          //schedulableStages.addLast(stageId);
-          schedulableStages.addFirst(stageId);
+          schedulableStages.addLast(stageId);
+          //schedulableStages.addFirst(stageId);
         }
       }
     }
@@ -213,7 +213,8 @@ public final class SingleJobTaskGroupQueue implements PendingTaskGroupQueue {
       schedulableStages.forEach(stageId -> result.append(stageId).append(", "));
     }
 
-    logs.add(result);
+    //logs.add(result);
+    LOG.info(result.toString());
   }
 
   public void printLog() {
