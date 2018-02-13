97a71550b2ffef5d99b39db388cb019931ee1869
Jae Hyeon Park <usezmap@gmail.com>
2018-02-13 12:22:55 +0900

    [Nemo-769] Fix `getPartitions` to work with compression (#775)
    * Fix `FileBlock#getPartitions` to skip unread bytes.
    * `LimitedInputStream` implementation for limiting overreading `FileInputStream`.
    * Add compression to `BlockStoreTest`.
    * Add `CompressionPass` in `PrimitiveCompositePass`.
    
    This PR fixes an issue about `DataSkewRuntimePass` not working properly with `CompressionPass`.
    
    Resolves #769

---
+++ runtime/executor/src/main/java/edu/snu/coral/runtime/executor/data/LimitedInputStream.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2018 Seoul National University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.snu.coral.runtime.executor.data;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * This class provide functionality to limit bytes read from {@link InputStream}.
+ * You need to wrap chained compression stream with this stream to prevent overreading
+ * inner stream.
+ */
+public final class LimitedInputStream extends InputStream {
+  private final InputStream in;
+  private long limit;
+
+  /**
+   * Constructor.
+   *
+   * @param in    {@link InputStream} that should be limited.
+   * @param limit bytes to limit.
+   * @throws IOException if failed to read stream.
+   */
+  public LimitedInputStream(final InputStream in, final long limit) {
+    this.in = in;
+    this.limit = limit;
+  }
+
+  @Override
+  public int read() throws IOException {
+    if (limit > 0) {
+      limit--;
+      return in.read();
+    } else {
+      return -1;
+    }
+  }
+}
