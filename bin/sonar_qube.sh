aab3f0470706562ce848a58ea575267f47975735
Won Wook SONG <wsong0512@gmail.com>
2018-03-25 00:18:55 +0900

    sonar qube script complete

--- bin/sonar_qube.sh
+++ bin/sonar_qube.sh
@@ -2,4 +2,5 @@
 
 echo "You should already have SonarQube installed and running at localhost:9000"
 echo "e.g. OSX: brew install sonarqube && sonar console"
+sonar console
 mvn clean package sonar:sonar
