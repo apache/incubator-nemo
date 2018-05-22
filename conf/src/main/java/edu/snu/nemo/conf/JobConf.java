99ff57960315a02da6465c582931c46aa59130b6
JangHo Seo <jangho@jangho.io>
2018-05-23 08:20:04 +0900

    adjust default value

--- conf/src/main/java/edu/snu/nemo/conf/JobConf.java
+++ conf/src/main/java/edu/snu/nemo/conf/JobConf.java
@@ -145,7 +145,7 @@ public final class JobConf extends ConfigurationModuleBuilder {
    * Number of maximum number of parallel downloads for a runtime edge.
    */
   @NamedParameter(doc = "Maximum number of parallel downloads for a runtime edge.", short_name = "max_downloads",
-      default_value = "3")
+      default_value = "30")
   public final class MaxNumDownloadsForARuntimeEdge implements Name<Integer> {
   }
 
