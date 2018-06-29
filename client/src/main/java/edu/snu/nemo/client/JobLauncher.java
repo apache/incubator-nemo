f4addea1fa4ad85d78c56a50e1c42e406ab39dcc
Jangho Seo <jangho@jangho.io>
2018-06-29 16:32:48 +0900

    getJSONConf

--- client/src/main/java/edu/snu/nemo/client/JobLauncher.java
+++ client/src/main/java/edu/snu/nemo/client/JobLauncher.java
@@ -33,6 +33,7 @@ import org.apache.reef.io.network.util.StringIdentifierFactory;
 import org.apache.reef.runtime.local.client.LocalRuntimeConfiguration;
 import org.apache.reef.runtime.yarn.client.YarnClientConfiguration;
 import org.apache.reef.tang.*;
+import org.apache.reef.tang.annotations.Name;
 import org.apache.reef.tang.exceptions.InjectionException;
 import org.apache.reef.tang.formats.CommandLine;
 import org.apache.reef.util.EnvironmentUtils;
@@ -95,7 +96,8 @@ public final class JobLauncher {
     final Configuration driverConf = getDriverConf(builtJobConf);
     final Configuration driverNcsConf = getDriverNcsConf();
     final Configuration driverMessageConfg = getDriverMessageConf();
-    final Configuration executorResourceConfig = getExecutorResourceConf(builtJobConf);
+    final Configuration executorResourceConfig = getJSONConf(builtJobConf, JobConf.ExecutorJsonPath.class,
+        JobConf.ExecutorJsonContents.class);
     final Configuration clientConf = getClientConf();
 
     // Merge Job and Driver Confs
@@ -277,19 +279,25 @@ public final class JobLauncher {
   }
 
   /**
-   * Get executor resource configuration.
+   * Read json file and return its contents as configuration parameter.
    *
-   * @param jobConf job configuration to get executor json path.
-   * @return executor resource configuration.
+   * @param jobConf job configuration to get json path.
+   * @param pathParameter named parameter represents path to the json file, or an empty string
+   * @param contentsParameter named parameter represents contents of the file
+   * @return configuration with contents of the file, or an empty string as value for {@code contentsParameter}
    * @throws InjectionException exception while injection.
    */
-  private static Configuration getExecutorResourceConf(final Configuration jobConf) throws InjectionException {
+  private static Configuration getJSONConf(final Configuration jobConf,
+                                           final Class<? extends Name<String>> pathParameter,
+                                           final Class<? extends Name<String>> contentsParameter)
+      throws InjectionException {
     final Injector injector = TANG.newInjector(jobConf);
     try {
-      final String path = injector.getNamedInstance(JobConf.ExecutorJsonPath.class);
-      final String contents = new String(Files.readAllBytes(Paths.get(path)), StandardCharsets.UTF_8);
+      final String path = injector.getNamedInstance(pathParameter);
+      final String contents = path.isEmpty() ? ""
+          : new String(Files.readAllBytes(Paths.get(path)), StandardCharsets.UTF_8);
       return TANG.newConfigurationBuilder()
-          .bindNamedParameter(JobConf.ExecutorJsonContents.class, contents)
+          .bindNamedParameter(contentsParameter, contents)
           .build();
     } catch (final IOException e) {
       throw new RuntimeException(e);
