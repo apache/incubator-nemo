64a09120e28e0817842d1861bed6e9289b8cd348
Jangho Seo <jangho@jangho.io>
2018-07-10 16:13:04 +0900

    [NEMO-71] Add NodeNamesAssignmentPass and Example Application (#62)
    JIRA: [NEMO-71: Add LocationShareAssignmentPass and Example Application](https://issues.apache.org/jira/projects/NEMO/issues/NEMO-71)
    
    **Major changes:**
    - Added NodeNamesAssignmentPass, which computes and assigns appropriate share of nodes to each vertex.
    - Add join example (NetworkTraceAnalysis).
    
    **Minor changes to note:**
    - N/A
    
    **Tests for the changes:**
    - Added integration test (NetworkTraceAnalysisITCase)
    
    **Other comments:**
    - N/A
    
    resolves [NEMO-71](https://issues.apache.org/jira/projects/NEMO/issues/NEMO-71)

---
+++ runtime/master/src/main/java/edu/snu/nemo/runtime/master/scheduler/NodeShareSchedulingConstraint.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2018 Seoul National University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.snu.nemo.runtime.master.scheduler;
+
+import edu.snu.nemo.common.ir.executionproperty.AssociatedProperty;
+import edu.snu.nemo.common.ir.vertex.executionproperty.NodeNamesProperty;
+import edu.snu.nemo.runtime.common.RuntimeIdGenerator;
+import edu.snu.nemo.runtime.common.plan.Task;
+import edu.snu.nemo.runtime.master.resource.ExecutorRepresenter;
+
+import javax.inject.Inject;
+import java.util.*;
+
+/**
+ * This constraint is to follow {@link NodeNamesProperty}.
+ */
+@AssociatedProperty(NodeNamesProperty.class)
+public final class NodeShareSchedulingConstraint implements SchedulingConstraint {
+
+  @Inject
+  private NodeShareSchedulingConstraint() {
+  }
+
+  private String getNodeName(final Map<String, Integer> propertyValue, final int taskIndex) {
+    final List<String> nodeNames = new ArrayList<>(propertyValue.keySet());
+    Collections.sort(nodeNames, Comparator.naturalOrder());
+    int index = taskIndex;
+    for (final String nodeName : nodeNames) {
+      if (index < propertyValue.get(nodeName)) {
+        index -= propertyValue.get(nodeName);
+      } else {
+        return nodeName;
+      }
+    }
+    throw new IllegalStateException("Detected excessive parallelism which NodeNamesProperty does not cover");
+  }
+
+  @Override
+  public boolean testSchedulability(final ExecutorRepresenter executor, final Task task) {
+    final Map<String, Integer> propertyValue = task.getPropertyValue(NodeNamesProperty.class)
+            .orElseThrow(() -> new RuntimeException("NodeNamesProperty expected"));
+    if (propertyValue.isEmpty()) {
+      return true;
+    }
+    return executor.getNodeName().equals(
+        getNodeName(propertyValue, RuntimeIdGenerator.getIndexFromTaskId(task.getTaskId())));
+  }
+}
