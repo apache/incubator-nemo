2a534586e75f6066361af168b90ace8b467dfdb7
skystar-p <usezmap@gmail.com>
2018-07-12 13:11:47 +0900

    dag with dagre d3 testing

--- components/DAG.vue
+++ components/DAG.vue
@@ -1,28 +1,46 @@
 <template>
   <div>
-    <p>{{ temp }}</p>
+    <svg id="dag-canvas"></svg>
+    <button @click="renderGraph()">Render</button>
   </div>
 </template>
 
 <script>
+import * as d3 from 'd3';
 import dagreD3 from 'dagre-d3';
 
 export default {
-  beforeMount() {
-    this.$eventBus.$on('wow', event => {
-      this.getEvent(event);
-    })
+  mounted() {
+    this.svg = d3.select('#dag-canvas');
+    this.svgGroup = this.svg.append('g');
+    this.render = new dagreD3.render();
+    this.graph = new dagreD3.graphlib.Graph({ compound: true, multigraph: true })
+      .setGraph({});
   },
 
   data() {
     return {
-      temp: 'wow',
+      svg: undefined,
+      svgGroup: undefined,
+      render: undefined,
+      graph: undefined,
     };
   },
 
   methods: {
-    getEvent(event) {
-      this.temp = event;
+    renderGraph() {
+      let g = this.graph;
+      g.setNode('a', { label: 'A' });
+      g.setNode('b', { label: 'B' });
+      g.setNode('c', { label: 'C' });
+      // g.setNode('group', { label: 'Stage', clusterLabelPos: 'top', style: 'fill: d3d7e8' });
+
+      // g.setParent('group', 'b');
+
+      g.setEdge('a', 'b', { label: 'ab' });
+      g.setEdge('b', 'c', { label: 'bc' });
+
+      this.render(d3.select('svg#dag-canvas g'), g);
     },
   }
 }
