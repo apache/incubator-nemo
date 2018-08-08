8a7533e79433852b8a9b110298ee27f8fc679434
Won Wook SONG <wsong0512@gmail.com>
2018-08-08 23:10:25 +0900

    checkstyle

--- common/src/main/java/edu/snu/nemo/common/dag/DAG.java
+++ common/src/main/java/edu/snu/nemo/common/dag/DAG.java
@@ -27,7 +27,6 @@ import java.io.PrintWriter;
 import java.io.Serializable;
 import java.util.*;
 import java.util.function.Consumer;
-import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
