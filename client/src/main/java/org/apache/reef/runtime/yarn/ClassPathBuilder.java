29f36f82d929d0e18f9c583269aafef345526bb0
Jangho Seo <jangho@jangho.io>
2018-10-26 15:53:11 +0900

    revert reef src

--- client/src/main/java/org/apache/reef/runtime/yarn/ClassPathBuilder.java
+++ client/src/main/java/org/apache/reef/runtime/yarn/ClassPathBuilder.java
@@ -16,9 +16,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
- * specific language governing permissions and limitations
- * under the License.
- */
 package org.apache.reef.runtime.yarn;
 
 import org.apache.reef.util.HadoopEnvironment;
