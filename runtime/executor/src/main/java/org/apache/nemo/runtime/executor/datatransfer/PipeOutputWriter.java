d2c5c4476f6319cf9d91fbc55b0e6f00d4b229ef
Taegeon Um <taegeonum@gmail.com>
2018-12-03 13:37:55 +0900

    [NEMO-293] OOM exception in streaming (#164)
    JIRA: [NEMO-293: OOM exception in streaming](https://issues.apache.org/jira/projects/NEMO/issues/NEMO-293)
    
    **Major changes:**
    - Fix wrong byte encoding in `PipeOutputWriter`. This causes OOM because it sends unnecessary bytes (count <= byte array size)
    - Add `writeElement` method to `ByteOutputContext` to emit data without copying byte array.

--- runtime/executor/src/main/java/org/apache/nemo/runtime/executor/datatransfer/PipeOutputWriter.java
+++ runtime/executor/src/main/java/org/apache/nemo/runtime/executor/datatransfer/PipeOutputWriter.java
@@ -18,14 +18,11 @@
  */
 package org.apache.nemo.runtime.executor.datatransfer;
 
-import org.apache.nemo.common.DirectByteArrayOutputStream;
-import org.apache.nemo.common.coder.EncoderFactory;
 import org.apache.nemo.common.ir.edge.executionproperty.CommunicationPatternProperty;
 import org.apache.nemo.common.punctuation.Watermark;
 import org.apache.nemo.runtime.common.RuntimeIdManager;
 import org.apache.nemo.runtime.common.plan.RuntimeEdge;
 import org.apache.nemo.runtime.executor.bytetransfer.ByteOutputContext;
-import org.apache.nemo.runtime.executor.data.DataUtil;
 import org.apache.nemo.runtime.executor.data.PipeManagerWorker;
 import org.apache.nemo.runtime.executor.data.partitioner.Partitioner;
 import org.apache.nemo.runtime.executor.data.streamchainer.Serializer;
@@ -33,7 +30,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.io.OutputStream;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -79,16 +75,7 @@ public final class PipeOutputWriter implements OutputWriter {
   private void writeData(final Object element, final List<ByteOutputContext> pipeList) {
     pipeList.forEach(pipe -> {
       try (final ByteOutputContext.ByteOutputStream pipeToWriteTo = pipe.newOutputStream()) {
-        // Serialize (Do not compress)
-        final DirectByteArrayOutputStream bytesOutputStream = new DirectByteArrayOutputStream();
-        final OutputStream wrapped =
-          DataUtil.buildOutputStream(bytesOutputStream, serializer.getEncodeStreamChainers());
-        final EncoderFactory.Encoder encoder = serializer.getEncoderFactory().create(wrapped);
-        encoder.encode(element);
-        wrapped.close();
-
-        // Write
-        pipeToWriteTo.write(bytesOutputStream.getBufDirectly());
+        pipeToWriteTo.writeElement(element, serializer);
       } catch (IOException e) {
         throw new RuntimeException(e); // For now we crash the executor on IOException
       }
