fb165c34186cf44fd2ca56e7d15819eae7e37b1f
taegeonum <taegeonum@gmail.com>
2018-12-10 09:43:04 +0900

    fix script

--- nex.sh
+++ nex.sh
@@ -23,9 +23,9 @@
 echo run query $1
 ./bin/run_nexmark.sh \
 	-job_id nexmark-Q$1 \
-	-executor_json `pwd`/examples/resources/beam_test_executor_resources.json \
+	-executor_json `pwd`/examples/resources/executors/beam_test_executor_resources.json \
 	-user_main org.apache.beam.sdk.nexmark.Main \
   -optimization_policy org.apache.nemo.compiler.optimizer.policy.StreamingPolicy \
   -scheduler_impl_class_name org.apache.nemo.runtime.master.scheduler.StreamingScheduler \
-	-user_args "--runner=org.apache.nemo.compiler.frontend.beam.NemoRunner --streaming=true --query=$1 --manageResources=false --monitorJobs=true --streamTimeout=400 --numEventGenerators=1 --numEvents=40000000 --isRateLimited=true --firstEventRate=100 --nextEventRate=100 --windowSizeSec=10 --windowPeriodSec=10 --fanout=1"
+	-user_args "--runner=org.apache.nemo.compiler.frontend.beam.NemoRunner --streaming=true --query=$1 --manageResources=false --monitorJobs=true --streamTimeout=400 --numEventGenerators=1 --numEvents=40000000 --isRateLimited=true --firstEventRate=100 --nextEventRate=100 --windowSizeSec=30 --windowPeriodSec=5 --fanout=1"
 
