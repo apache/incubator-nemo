efa9d96d5a7754c4e202a4a97718556d8b244907
taegeonum <taegeonum@gmail.com>
2018-12-14 09:18:05 +0900

    fix

---
+++ runtime/executor/src/main/java/org/apache/nemo/runtime/executor/datatransfer/GBKLambdaEmitter.java
@@ -0,0 +1,30 @@
+package org.apache.nemo.runtime.executor.datatransfer;
+
+import org.apache.nemo.common.ir.OutputCollector;
+import org.apache.nemo.common.punctuation.Watermark;
+import org.apache.nemo.runtime.executor.data.SerializerManager;
+
+
+public class GBKLambdaEmitter implements OutputCollector {
+
+
+
+  public GBKLambdaEmitter(final SerializerManager serializerManager) {
+
+  }
+
+  @Override
+  public void emit(Object output) {
+
+  }
+
+  @Override
+  public void emitWatermark(Watermark watermark) {
+
+  }
+
+  @Override
+  public void emit(String dstVertexId, Object output) {
+
+  }
+}
