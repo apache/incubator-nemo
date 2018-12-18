9d98d68f0d65dabdc11d37958acf6808a7b63d56
taegeonum <taegeonum@gmail.com>
2018-12-18 10:36:49 +0900

    addr

--- runtime/executor/src/main/java/org/apache/nemo/runtime/executor/datatransfer/NettyServerLambdaTransport.java
+++ runtime/executor/src/main/java/org/apache/nemo/runtime/executor/datatransfer/NettyServerLambdaTransport.java
@@ -36,7 +36,7 @@ public final class NettyServerLambdaTransport {
   private static final int SERVER_WORKER_NUM_THREADS = 10;
   private static final String CLASS_NAME = NettyServerLambdaTransport.class.getName();
   private static final String ADDRESS = "172.31.6.35";
-  private static final String PUBLIC_ADDRESS = "18.182.36.12";
+  private static final String PUBLIC_ADDRESS = "13.231.167.215";
   private static final int PORT = 20332;
 
   private final ChannelGroup serverChannelGroup = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
