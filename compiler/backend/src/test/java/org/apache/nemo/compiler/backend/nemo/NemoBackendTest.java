a5f22f0a391474046e25fcd05acb004c8dea271a
John Yang <johnyangk@apache.org>
2019-01-22 18:00:41 +0900

    unit tests pass

--- compiler/backend/src/test/java/org/apache/nemo/compiler/backend/nemo/NemoBackendTest.java
+++ compiler/backend/src/test/java/org/apache/nemo/compiler/backend/nemo/NemoBackendTest.java
@@ -19,6 +19,7 @@
 package org.apache.nemo.compiler.backend.nemo;
 
 import org.apache.nemo.common.dag.DAG;
+import org.apache.nemo.common.ir.IRDAG;
 import org.apache.nemo.common.ir.edge.IREdge;
 import org.apache.nemo.common.ir.edge.executionproperty.CommunicationPatternProperty;
 import org.apache.nemo.common.ir.vertex.IRVertex;
@@ -48,16 +49,16 @@ public final class NemoBackendTest<I, O> {
   private NemoBackend nemoBackend;
 
   private final DAGBuilder<IRVertex, IREdge> builder = new DAGBuilder<>();
-  private DAG<IRVertex, IREdge> dag;
+  private IRDAG dag;
 
   @Before
   public void setUp() throws Exception {
-    this.dag = builder.addVertex(source).addVertex(map1).addVertex(groupByKey).addVertex(combine).addVertex(map2)
+    this.dag = new IRDAG(builder.addVertex(source).addVertex(map1).addVertex(groupByKey).addVertex(combine).addVertex(map2)
         .connectVertices(new IREdge(CommunicationPatternProperty.Value.OneToOne, source, map1))
         .connectVertices(new IREdge(CommunicationPatternProperty.Value.Shuffle, map1, groupByKey))
         .connectVertices(new IREdge(CommunicationPatternProperty.Value.OneToOne, groupByKey, combine))
         .connectVertices(new IREdge(CommunicationPatternProperty.Value.OneToOne, combine, map2))
-        .build();
+        .build());
 
     this.dag = new TransientResourcePolicy().runCompileTimeOptimization(dag, EMPTY_DAG_DIRECTORY);
 
