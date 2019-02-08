623233556ef216cc6342182b94bef9b0290efb6e
taegeonum <taegeonum@gmail.com>
2019-02-08 16:10:53 +0900

    fix constnats

--- common/src/main/java/org/apache/nemo/common/lambda/Constants.java
+++ common/src/main/java/org/apache/nemo/common/lambda/Constants.java
@@ -10,5 +10,5 @@ public  class Constants {
 
   public static final String SIDEINPUT_LAMBDA_NAME2 = "nemo-dev-imhandler";
 
-  public static final int FLUSH_BYTES = 5 * 1024 * 1024; // MB
+  public static final int FLUSH_BYTES = 10 * 1024 * 1024; // MB
 }
