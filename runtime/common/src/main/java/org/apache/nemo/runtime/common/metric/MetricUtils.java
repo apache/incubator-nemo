522cbe9e606749372b9b253f5632409ed56a2d1c
Won Wook SONG <wsong0512@gmail.com>
2019-02-11 12:11:24 +0900

    add hashcode helpers

--- runtime/common/src/main/java/org/apache/nemo/runtime/common/metric/MetricUtils.java
+++ runtime/common/src/main/java/org/apache/nemo/runtime/common/metric/MetricUtils.java
@@ -19,6 +19,7 @@
 
 package org.apache.nemo.runtime.common.metric;
 
+import org.apache.nemo.common.Pair;
 import org.apache.nemo.common.exception.MetricException;
 import org.apache.nemo.common.ir.IRDAG;
 import org.apache.nemo.common.ir.executionproperty.ExecutionProperty;
@@ -27,12 +28,17 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Stream;
 
 /**
  * Utility class for metrics.
  */
 public final class MetricUtils {
+  private static final Map<Integer, Pair<String, Class<? extends ExecutionProperty>>>
+    HASHCODE_TO_ID_AND_EP_KEY = new HashMap<>();
+  private static final Map<Integer, ExecutionProperty<?>> HASHCODE_TO_EXECUTION_PROPERTY = new HashMap<>();
 
   /**
    * Private constructor.
@@ -49,7 +55,7 @@ public final class MetricUtils {
     final StringBuilder builder = new StringBuilder();
     irdag.getVertices().forEach(v ->
       v.getExecutionProperties().forEachProperties(ep ->
-        epFormatter(builder, v.getNumericId(), ep)));
+        epFormatter(builder, v.getId(), v.getNumericId(), ep)));
     return builder.toString().trim();
   }
 
@@ -63,23 +69,48 @@ public final class MetricUtils {
     irdag.getVertices().forEach(v ->
       irdag.getIncomingEdgesOf(v).forEach(e ->
         e.getExecutionProperties().forEachProperties(ep ->
-          epFormatter(builder, v.getNumericId(), ep))));
+          epFormatter(builder, e.getId(), e.getNumericId(), ep))));
     return builder.toString().trim();
   }
 
   /**
    * Formatter for execution properties.
    * @param builder string builder to append the metrics to.
+   * @param id ID of the vertex or the edge.
    * @param numericId numeric ID of the vertex or the edge.
    * @param ep the execution property.
    */
-  private static void epFormatter(final StringBuilder builder, final Integer numericId, final ExecutionProperty<?> ep) {
+  private static void epFormatter(final StringBuilder builder, final String id,
+                                  final Integer numericId, final ExecutionProperty<?> ep) {
     builder.append(numericId);
-    builder.append(0);
-    builder.append(ep.getClass().hashCode());
+    builder.append("0");
+    final int classHashcode = ep.getClass().hashCode();
+    HASHCODE_TO_ID_AND_EP_KEY.putIfAbsent(classHashcode, Pair.of(id, ep.getClass()));
+    builder.append(classHashcode);
+
     builder.append(":");
-    builder.append(ep.getValue());
+    final int valueHashcode = ep.hashCode();
+    HASHCODE_TO_EXECUTION_PROPERTY.putIfAbsent(valueHashcode, ep);
+    builder.append(valueHashcode);
     builder.append(" ");
+  }
+
+  /**
+   * Get the vertex/edge id and the execution property class (key) by the hashcode.
+   * @param hashcode the hashcode.
+   * @return the pair of id and the execution property.
+   */
+  public static Pair<String, Class<? extends ExecutionProperty>> getIdAndEPKeyByHashcode(final int hashcode) {
+    return HASHCODE_TO_ID_AND_EP_KEY.get(hashcode);
+  }
+
+  /**
+   * Get the execution property by the hashcode.
+   * @param hashcode the hashcode.
+   * @return the execution property.
+   */
+  public static ExecutionProperty<?> getExecutionPropertyByHashcode(final int hashcode) {
+    return HASHCODE_TO_EXECUTION_PROPERTY.get(hashcode);
   }
 
   /**
