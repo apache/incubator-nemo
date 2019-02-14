afb8778fcd45f62ff9b993b0d83239648c32add0
Haeyoon Cho <chohy0555@gmail.com>
2019-02-14 15:50:32 +0900

    logging

--- runtime/executor/src/main/java/org/apache/nemo/runtime/executor/data/block/FileBlock.java
+++ runtime/executor/src/main/java/org/apache/nemo/runtime/executor/data/block/FileBlock.java
@@ -96,8 +96,9 @@ public final class FileBlock<K extends Serializable> implements Block<K> {
       try {
         this.fs = fs;
         this.file = fs.create(filePath+'/'+id, CrailNodeType.DATAFILE, CrailStorageClass.DEFAULT, CrailLocationClass.DEFAULT, true).get().asFile();
-        LOG.info("HY:crail file created");
+        LOG.info("HY: crail file block created");
       } catch (Exception e) {
+        LOG.info("HY: crail file block creation failed");
         e.printStackTrace();
       }
     }
