427db919bacf73bab59c9a6ad4f0b499f7fa4d24
Haeyoon Cho <chohy0555@gmail.com>
2019-02-16 14:37:47 +0900

    file creation

--- runtime/executor/src/main/java/org/apache/nemo/runtime/executor/data/stores/CrailFileStore.java
+++ runtime/executor/src/main/java/org/apache/nemo/runtime/executor/data/stores/CrailFileStore.java
@@ -66,13 +66,13 @@ public final class CrailFileStore extends AbstractBlockStore implements RemoteFi
                          @Parameter(JobConf.JobId.class) final String jobId,
                          final SerializerManager serializerManager) throws Exception {
     super(serializerManager);
-    this.fileDirectory =volumeDirectory + "/" + jobId;
-    new File(fileDirectory).mkdirs();
+    this.fileDirectory = volumeDirectory + "/" + jobId;
+    //new File(fileDirectory).mkdirs();
     this.conf = new CrailConfiguration();
     this.fs = CrailStore.newInstance(conf);
     //LOG.info("HY: CrailStore created. Not yet for the file directory itself");
     //Parent Node (/tmp_crail/jobId/) creation needed
-    //fs.create(fileDirectory, CrailNodeType.DIRECTORY, CrailStorageClass.DEFAULT, CrailLocationClass.DEFAULT, false);
+    fs.create(fileDirectory, CrailNodeType.DIRECTORY, CrailStorageClass.DEFAULT, CrailLocationClass.DEFAULT, true).get().syncDir();
   }
 
   @Override
