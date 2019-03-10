5a11b3c1aae5fc094b73d94eebfe779053e64010
taegeonum <taegeonum@gmail.com>
2019-03-10 11:23:18 +0900

    logging

--- client/src/main/java/org/apache/nemo/client/JobLauncher.java
+++ client/src/main/java/org/apache/nemo/client/JobLauncher.java
@@ -250,7 +250,7 @@ public final class JobLauncher {
       Thread.currentThread().interrupt();
     }
 
-    LOG.info("Launching DAG...");
+    LOG.info("Launching DAG...: {}", dag);
     serializedDAG = Base64.getEncoder().encodeToString(SerializationUtils.serialize(dag));
     jobDoneLatch = new CountDownLatch(1);
     driverRPCServer.send(ControlMessage.ClientToDriverMessage.newBuilder()
