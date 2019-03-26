b00ba7a022980fe4a0dcdd106549348a1ef9397c
Haeyoon Cho <chohy0555@gmail.com>
2019-03-26 21:50:02 +0900

    checkstyle

--- common/src/main/java/org/apache/nemo/common/DirectByteArrayOutputStream.java
+++ common/src/main/java/org/apache/nemo/common/DirectByteArrayOutputStream.java
@@ -44,7 +44,7 @@ public final class DirectByteArrayOutputStream extends ByteArrayOutputStream {
 
   /**
    * Note that serializedBytes include invalid bytes.
-   * So we have to use it with the actualLength by using size() whenever needed
+   * So we have to use it with the actualLength by using size() whenever needed.
    * @return the buffer where data is stored.
    */
   public byte[] getBufDirectly() {
