de67b774ab81c3a2533cc1ba28114d9651d64e1f
taegeonum <taegeonum@gmail.com>
2019-05-22 10:02:30 +0900

    flush fixgs

--- runtime/lambda-executor/src/main/java/org/apache/nemo/runtime/lambdaexecutor/kafka/KafkaOffloadingOutput.java
+++ runtime/lambda-executor/src/main/java/org/apache/nemo/runtime/lambdaexecutor/kafka/KafkaOffloadingOutput.java
@@ -1,19 +1,25 @@
 package org.apache.nemo.runtime.lambdaexecutor.kafka;
 
 import org.apache.beam.sdk.io.UnboundedSource;
+import org.apache.nemo.compiler.frontend.beam.transform.GBKFinalState;
+
+import java.util.Map;
 
 public final class KafkaOffloadingOutput {
 
   public final String taskId;
   public final int id;
   public final UnboundedSource.CheckpointMark checkpointMark;
+  public final Map<String, GBKFinalState> stateMap;
 
   public KafkaOffloadingOutput(
     final String taskId,
     final int id,
-    final UnboundedSource.CheckpointMark checkpointMark) {
+    final UnboundedSource.CheckpointMark checkpointMark,
+    final Map<String, GBKFinalState> stateMap) {
     this.taskId = taskId;
     this.id = id;
     this.checkpointMark = checkpointMark;
+    this.stateMap = stateMap;
   }
 }
