77bab9716d7d331feaa4f41b72faa76d9ad89ede
taegeonum <taegeonum@gmail.com>
2019-05-23 09:56:01 +0900

    fix relay coding

--- runtime/executor-common/src/main/java/org/apache/nemo/runtime/executor/common/relayserverclient/RelayUtils.java
+++ runtime/executor-common/src/main/java/org/apache/nemo/runtime/executor/common/relayserverclient/RelayUtils.java
@@ -1,14 +1,7 @@
 package org.apache.nemo.runtime.executor.common.relayserverclient;
 
-import java.io.ByteArrayOutputStream;
 
 public class RelayUtils {
-
-
-  public byte[] encodeDstId(final String dstId) {
-    final byte[] bytes = new byte[30];
-    final int l = dstId.length();
-  }
 
   public static String createId(final String edgeId,
                                 final int taskIndex,
