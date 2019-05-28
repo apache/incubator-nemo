f7dde3c59f2020f42e0566b3d0ade792618bf756
taegeonum <taegeonum@gmail.com>
2019-05-28 20:56:08 +0900

    watermark logging

--- runtime/executor-common/src/main/java/org/apache/nemo/runtime/executor/common/DynamicInputWatermarkManager.java
+++ runtime/executor-common/src/main/java/org/apache/nemo/runtime/executor/common/DynamicInputWatermarkManager.java
@@ -100,7 +100,8 @@ public final class DynamicInputWatermarkManager implements InputWatermarkManager
   public synchronized void trackAndEmitWatermarks(final int edgeIndex, final Watermark watermark) {
 
     if (vertex != null) {
-      LOG.info("Watermark from {}: {} at {}, min: {}", edgeIndex, new Instant(watermark.getTimestamp()), vertex.getId(), currMinWatermark);
+      LOG.info("Watermark from {}: {} at {}, min: {}, minIndex: {}", edgeIndex, new Instant(watermark.getTimestamp()), vertex.getId(), currMinWatermark,
+        minWatermarkIndex);
       LOG.info("Print watermarks");
       printWatermarks();
     }
@@ -115,6 +116,10 @@ public final class DynamicInputWatermarkManager implements InputWatermarkManager
         // find min watermark
         final int nextMinWatermarkIndex = findNextMinWatermarkIndex();
         final Watermark nextMinWatermark = taskWatermarkMap.get(nextMinWatermarkIndex);
+
+        LOG.info("nextMin: {}, netMinIndex: {}, currMin: {} at {}", new Instant(nextMinWatermark.getTimestamp()),
+          nextMinWatermarkIndex,
+          new Instant(currMinWatermark.getTimestamp()), vertex.getId());
 
         if (nextMinWatermark.getTimestamp() < currMinWatermark.getTimestamp()) {
           // it is possible
