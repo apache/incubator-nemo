36aadab027edf24dda717dba8dd2cfe3a8cc4806
taegeonum <taegeonum@gmail.com>
2019-07-01 16:36:28 +0900

    Revert "kafka polling"
    This reverts commit a5c2b378074178104373ff5b5e65c14454fb2079.

--- compiler/frontend/beam/src/main/java/org/apache/nemo/compiler/frontend/beam/source/UnboundedSourceReadable.java
+++ compiler/frontend/beam/src/main/java/org/apache/nemo/compiler/frontend/beam/source/UnboundedSourceReadable.java
@@ -1,8 +1,6 @@
 package org.apache.nemo.compiler.frontend.beam.source;
 
 import org.apache.beam.sdk.io.UnboundedSource;
-import org.apache.beam.sdk.io.kafka.KafkaUnboundedReader;
-import org.apache.beam.sdk.io.kafka.KafkaUnboundedSource;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.transforms.windowing.GlobalWindow;
 import org.apache.beam.sdk.util.WindowedValue;
@@ -37,9 +35,7 @@ public final class UnboundedSourceReadable<O, M extends UnboundedSource.Checkpoi
 
   private static final Logger LOG = LoggerFactory.getLogger(UnboundedSourceReadable.class.getName());
 
-
-  private KafkaUnboundedReader kafkaReader;
-
+  private ExecutorService readableService;
   /**
    * Constructor.
    * @param unboundedSource unbounded source.
@@ -68,8 +64,8 @@ public final class UnboundedSourceReadable<O, M extends UnboundedSource.Checkpoi
   public void prepare() {
     LOG.info("Prepare unbounded sources!! {}, {}", unboundedSource, unboundedSource.toString());
     try {
+      readableService = ReadableService.getInstance();
       reader = unboundedSource.createReader(pipelineOptions, checkpointMark);
-      kafkaReader = (KafkaUnboundedReader) reader;
       isCurrentAvailable = reader.start();
       isFetchTime = !isCurrentAvailable;
 
@@ -96,14 +92,13 @@ public final class UnboundedSourceReadable<O, M extends UnboundedSource.Checkpoi
       return new TimestampAndValue<>(currTs.getMillis(),
         WindowedValue.timestampedValueInGlobalWindow(elem, reader.getCurrentTimestamp()));
     } else {
-      LOG.info("Calling kafkaReader.pollRecord");
-      kafkaReader.pollRecord(5);
        try {
         isCurrentAvailable =  reader.advance();
       } catch (IOException e) {
         e.printStackTrace();
         throw new RuntimeException(e);
       }
+
       return EmptyElement.getInstance();
     }
 
