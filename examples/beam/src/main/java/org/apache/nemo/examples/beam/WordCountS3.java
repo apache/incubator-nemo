5101fcf4b2ac75c082eb4ecdc0332935f89f3d03
alapha23 <alapha23@gmail.com>
2019-09-02 14:24:07 +0900

    Update WordCountS3 example

--- examples/beam/src/main/java/org/apache/nemo/examples/beam/WordCountS3.java
+++ examples/beam/src/main/java/org/apache/nemo/examples/beam/WordCountS3.java
@@ -1,9 +1,14 @@
 package org.apache.nemo.examples.beam;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.PipelineResult;
+import org.apache.beam.sdk.PipelineRunner;
+import org.apache.beam.sdk.io.FileIO;
 import org.apache.beam.sdk.io.TextIO;
-import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.transforms.SimpleFunction;
 import org.apache.beam.sdk.values.PCollection;
@@ -15,12 +20,22 @@ public class WordCountS3 {
   private static final Logger log = LoggerFactory.getLogger(WordCountS3.class);
 
   public static void main(String[] args) {
-    final PipelineOptions options = NemoPipelineOptionsFactory.create();
-    options.setJobName("WordCountS3");
+    WordCountS3Option options = PipelineOptionsFactory.fromArgs(args).withValidation().as(WordCountS3Option.class);
+    try {
+      options.setRunner((Class<? extends PipelineRunner<?>>)
+        Class.forName("org.apache.nemo.client.beam.NemoRunner"));
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
     Pipeline pipeline = Pipeline.create(options);
 
+    AWSCredentials awsCredentials = new BasicAWSCredentials(options.getAWSAccessKey().get(),
+      options.getAWSSecretKey().get());
+    options.setAwsCredentialsProvider(new AWSStaticCredentialsProvider(awsCredentials));
+
     PCollection<String> fileLines =
-      pipeline.apply("ReadFromFile", TextIO.read().from("s3://lambda-executor-examples/sample_1.csv"));
+      pipeline.apply("ReadFromFile", TextIO.read().from("s3://lambda-executor-examples/sample1.csv"));
+//      pipeline.apply("ReadFromFile", TextIO.read().withMatchConfiguration(FileIO.MatchConfiguration.create()).from("s3://lambda-executor-examples/sample1.csv"));
 
     fileLines.apply("PrintLines", MapElements.via(new SimpleFunction<String, Void>() {
       @Override
--- examples/beam/src/main/java/org/apache/nemo/examples/beam/WordCountS3Option.java
+++ examples/beam/src/main/java/org/apache/nemo/examples/beam/WordCountS3Option.java
@@ -1,4 +1,19 @@
 package org.apache.nemo.examples.beam;
 
-public class WordCountS3Option {
+
+import org.apache.beam.sdk.io.aws.options.S3Options;
+import org.apache.beam.sdk.options.*;
+import org.apache.nemo.compiler.frontend.beam.NemoPipelineOptions;
+
+public interface WordCountS3Option extends PipelineOptions, S3Options {
+  @Description("AWS Access Key")
+  @Validation.Required
+  ValueProvider<String> getAWSAccessKey();
+  void setAWSAccessKey(ValueProvider<String> value);
+
+  @Description("AWS secret key")
+  @Validation.Required
+  ValueProvider<String> getAWSSecretKey();
+  void setAWSSecretKey(ValueProvider<String> value);
 }
+
