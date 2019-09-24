36b2bbd77846311132b9e953f3864a5c6df8c32b
John Yang <johnyangk@apache.org>
2019-09-24 15:34:05 +0900

    no store in transient resource pass

--- compiler/optimizer/src/main/java/org/apache/nemo/compiler/optimizer/pass/compiletime/annotating/TransientResourceDataTransferPass.java
+++ compiler/optimizer/src/main/java/org/apache/nemo/compiler/optimizer/pass/compiletime/annotating/TransientResourceDataTransferPass.java
@@ -50,15 +50,9 @@ public final class TransientResourceDataTransferPass extends AnnotatingPass {
       if (!inEdges.isEmpty()) {
         inEdges.forEach(edge -> {
           if (fromTransientToReserved(edge)) {
-            if (!Optional.of(DataStoreProperty.Value.SERIALIZED_MEMORY_STORE)
-              .equals(edge.getPropertyValue(DataStoreProperty.class))) {
-              edge.setPropertyPermanently(DataStoreProperty.of(DataStoreProperty.Value.MEMORY_STORE));
-            }
             edge.setPropertyPermanently(DataFlowProperty.of(DataFlowProperty.Value.PUSH));
             edge.setPropertyPermanently(BlockFetchFailureProperty.of(
               BlockFetchFailureProperty.Value.RETRY_AFTER_TWO_SECONDS_FOREVER));
-          } else if (fromReservedToTransient(edge)) {
-            edge.setPropertyPermanently(DataStoreProperty.of(DataStoreProperty.Value.LOCAL_FILE_STORE));
           }
         });
       }
