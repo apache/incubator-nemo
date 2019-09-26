59a8a181e91c8a4c15a9fde91e9a2fda54845c6f
taegeonum <taegeonum@gmail.com>
2019-09-26 16:05:15 +0900

    rm log

--- runtime/executor/src/main/java/org/apache/nemo/runtime/executor/TinyTaskOffloadingWorkerManager.java
+++ runtime/executor/src/main/java/org/apache/nemo/runtime/executor/TinyTaskOffloadingWorkerManager.java
@@ -139,7 +139,7 @@ public final class TinyTaskOffloadingWorkerManager<I, O> implements ServerlessEx
 
         if (msg instanceof OffloadingHeartbeatEvent) {
           final OffloadingHeartbeatEvent heartbeatEvent = (OffloadingHeartbeatEvent) msg;
-          LOG.info("Set task metrics from sf", heartbeatEvent.taskMetrics);
+          //LOG.info("Set task metrics from sf", heartbeatEvent.taskMetrics);
 
           for (final Pair<String, TaskMetrics.RetrievedMetrics> metric : heartbeatEvent.taskMetrics) {
             sfTaskMetrics.sfTaskMetrics.put(metric.left(), metric.right());
