ffb29101dc176993e5a15b5027614a56dcf708bf
taegeonum <taegeonum@gmail.com>
2019-09-28 16:33:44 +0900

    random delay

--- offloading/client/src/main/java/org/apache/nemo/offloading/client/VMOffloadingRequester.java
+++ offloading/client/src/main/java/org/apache/nemo/offloading/client/VMOffloadingRequester.java
@@ -235,6 +235,15 @@ public final class VMOffloadingRequester {
               final StartInstancesRequest startRequest = new StartInstancesRequest()
                 .withInstanceIds(instanceId);
               LOG.info("Starting ec2 instances {}/{}", instanceId, System.currentTimeMillis());
+              final Random random = new Random();
+              final double d = random.nextDouble();
+
+              try {
+                Thread.sleep((long)(1300* d));
+              } catch (InterruptedException e) {
+                e.printStackTrace();
+              }
+
               ec2.startInstances(startRequest);
               LOG.info("End of Starting ec2 instances {}/{}", instanceId, System.currentTimeMillis());
               return;
