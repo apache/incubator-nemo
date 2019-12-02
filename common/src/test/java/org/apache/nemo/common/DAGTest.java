affa22278e88aff27ab566dfd67e4a1dc97dbea3
Chaemin <chaaaamni@gmail.com>
2019-12-02 18:07:07 +0900

    Fix arguments correct order

--- common/src/test/java/org/apache/nemo/common/DAGTest.java
+++ common/src/test/java/org/apache/nemo/common/DAGTest.java
@@ -106,8 +106,8 @@ public final class DAGTest {
     final List<IntegerVertex> topologicalOrder = dag.getTopologicalSort();
     assertEquals(topologicalOrder.get(0).getValue(), 4);
     assertEquals(topologicalOrder.get(1).getValue(), 5);
-    assertEquals(topologicalOrder.get(2).getValue(), 1);
-    assertEquals(topologicalOrder.get(3).getValue(), 2);
+    assertEquals(1, topologicalOrder.get(2).getValue());
+    assertEquals(2, topologicalOrder.get(3).getValue());
     assertEquals(topologicalOrder.get(4).getValue(), 3);
 
     assertEquals(dag.getRootVertices().size(), 2);
