31ba77a43ff03b64359c6799306c7a28770bfd13
zlzlqlzl <zlzlqlzl1@naver.com>
2019-12-02 17:57:00 +0900

    code smell fixed

--- common/src/main/java/org/apache/nemo/common/coder/LongEncoderFactory.java
+++ common/src/main/java/org/apache/nemo/common/coder/LongEncoderFactory.java
@@ -60,6 +60,7 @@ public final class LongEncoderFactory implements EncoderFactory<Long> {
    */
   private final class LongEncoder implements Encoder<Long> {
 
+    @java.lang.SuppressWarnings("squid:S1948")
     private final DataOutputStream outputStream;
 
     /**
