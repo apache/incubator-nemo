33fe65af80303f9b742bed925a78e0b4bd29d445
Taeyoung Jang <tyj9327@gmail.com>
2019-12-02 18:04:38 +0900

    Resolve code smell
    - runtime/executor/src/main/java/org/apache/nemo/runtime/executor/data/stores/LocalFileStore.java
    - row 79
    - removed throws BlockWriteException

--- runtime/executor/src/main/java/org/apache/nemo/runtime/executor/data/stores/LocalFileStore.java
+++ runtime/executor/src/main/java/org/apache/nemo/runtime/executor/data/stores/LocalFileStore.java
@@ -76,7 +76,7 @@ public final class LocalFileStore extends LocalBlockStore {
    * @throws BlockWriteException if fail to write.
    */
   @Override
-  public void writeBlock(final Block block) throws BlockWriteException {
+  public void writeBlock(final Block block) {
     if (!(block instanceof FileBlock)) {
       throw new BlockWriteException(new Throwable(
         this.toString() + "only accept " + FileBlock.class.getName()));
