a89b2249fcb1d544f8c8930dbae697adaf6c8c09
peter <peterpan1998@snu.ac.kr>
2019-12-02 17:52:16 +0900

    Fixed another code smell..??

--- common/src/test/java/org/apache/nemo/common/DAGTest.java
+++ common/src/test/java/org/apache/nemo/common/DAGTest.java
@@ -78,7 +78,7 @@ public final class DAGTest {
 
     assertEquals(dag.getRootVertices().size(), 1);
     assertEquals(0, dag.getParents("1").size());
-    assertEquals(dag.getChildren("1").size(), 1);
+    assertEquals(1, dag.getChildren("1").size());
     assertEquals(dag.getParents("2").size(), 1);
     assertEquals(dag.getChildren("2").size(), 1);
     assertEquals(dag.getParents("5").size(), 1);
