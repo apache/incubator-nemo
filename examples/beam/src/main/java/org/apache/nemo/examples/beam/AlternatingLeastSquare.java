d57fd4a4dd8618c640b97a6e04c17cb5469f26a5
kdh9949 <kdh9949@snu.ac.kr>
2019-12-02 18:11:30 +0900

    add NOSONAR

--- examples/beam/src/main/java/org/apache/nemo/examples/beam/AlternatingLeastSquare.java
+++ examples/beam/src/main/java/org/apache/nemo/examples/beam/AlternatingLeastSquare.java
@@ -268,8 +268,8 @@ public final class AlternatingLeastSquare {
     extends LoopCompositeTransform<PCollection<KV<Integer, float[]>>, PCollection<KV<Integer, float[]>>> {
     private final Integer numFeatures;
     private final double lambda;
-    private final PCollection<KV<Integer, KV<int[], float[]>>> parsedUserData;
-    private final PCollection<KV<Integer, KV<int[], float[]>>> parsedItemData;
+    private final PCollection<KV<Integer, KV<int[], float[]>>> parsedUserData; // NOSONAR
+    private final PCollection<KV<Integer, KV<int[], float[]>>> parsedItemData; // NOSONAR
 
     /**
      * Constructor of UpdateUserAndItemMatrix CompositeTransform.
