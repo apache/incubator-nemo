0cab1dc36f5193a223b0ea448d7a41f9732ef06e
Sanha Lee <sanha@apache.org>
2020-02-13 13:40:38 +0900

    [NEMO-421] Release v0.2 (#286)
    JIRA: [NEMO-421: Release v0.2](https://issues.apache.org/jira/projects/NEMO/issues/NEMO-421)
    
    **Major changes:**
    - Update the versions of components according to the release v0.2.
    
    **Minor changes to note:**
    - N/A
    
    **Tests for the changes:**
    - N/A
    
    **Other comments:**
    - I'll open a release vote current version.  If it passed, I'll update the versions to `0.3-SNAPSHOT` and merge this PR. Please do not merge any other PR before the vote. Thanks :)
    
    Closes #421

--- compiler/optimizer/src/main/java/org/apache/nemo/compiler/optimizer/examples/MapReduceDisaggregationOptimization.java
+++ compiler/optimizer/src/main/java/org/apache/nemo/compiler/optimizer/examples/MapReduceDisaggregationOptimization.java
@@ -47,7 +47,6 @@ public final class MapReduceDisaggregationOptimization {
    * Main function of the example MR program.
    *
    * @param args arguments.
-   * @throws Exception Exceptions on the way.
    */
   public static void main(final String[] args) {
     final IRVertex source = new EmptyComponents.EmptySourceVertex<>("Source");
