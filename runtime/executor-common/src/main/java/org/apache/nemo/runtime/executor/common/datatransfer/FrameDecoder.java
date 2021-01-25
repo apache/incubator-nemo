6532090691971d3a42a02224b4f4220657c0d670
taegeonum <taegeonum@gmail.com>
2021-01-25 10:30:51 +0900

    fix

--- runtime/executor-common/src/main/java/org/apache/nemo/runtime/executor/common/datatransfer/FrameDecoder.java
+++ runtime/executor-common/src/main/java/org/apache/nemo/runtime/executor/common/datatransfer/FrameDecoder.java
@@ -242,15 +242,16 @@ public final class FrameDecoder extends ByteToMessageDecoder {
       throw new RuntimeException("Data body bytes zero");
     }
 
+
+    if (in.readableBytes() < dataBodyBytesToRead) {
+      LOG.warn("Bytes to read smaller than dataBodyBytesToRead: "
+        + in.readableBytes() + ", " + dataBodyBytesToRead);
+      return false;
+    }
+
     // length should not exceed Integer.MAX_VALUE (since in.readableBytes() returns an int)
     final long length = Math.min(dataBodyBytesToRead, in.readableBytes());
     assert (length <= Integer.MAX_VALUE);
-
-    if (dataBodyBytesToRead < in.readableBytes()) {
-      LOG.warn("Data body bytes to read smaller than readableBytes: "
-        + dataBodyBytesToRead + ", " + in.readableBytes());
-      return false;
-    }
 
     // final ByteBuf body = in.readSlice((int) length).retain();
     final ByteBuf buf = in.readRetainedSlice((int) length);
@@ -258,30 +259,27 @@ public final class FrameDecoder extends ByteToMessageDecoder {
     // dataByteBufs.add(buf);
     dataBodyBytesToRead -= length;
 
-    if (dataBodyBytesToRead == 0) {
-      // Merge into bytebuf
-      // final CompositeByteBuf compositeByteBuf =
-      //  ctx.alloc().compositeBuffer(dataByteBufs.size()).addComponents(true, dataByteBufs);
-      //dataByteBufs = new LinkedList<>();
+    if (dataBodyBytesToRead != 0) {
+      throw new RuntimeException(("DataBodyByesToRead should be zero"));
+    }
 
-      if (inputContext == null) {
-        LOG.info("Add bytebuf for null transferIndex {}", currTransferIndex);
-        pendingByteBufMap.putIfAbsent(currTransferIndex, new LinkedList<>());
-        pendingByteBufMap.get(currTransferIndex).add(buf);
-      } else {
-        if (pendingByteBufMap.containsKey(currTransferIndex)) {
-          LOG.info("Flushing pending bytebuf for transferIndex {}", currTransferIndex);
-          for (final ByteBuf pendingByte : pendingByteBufMap.remove(currTransferIndex)) {
-            inputContext.onByteBuf(pendingByte);
-          }
+    if (inputContext == null) {
+      LOG.info("Add bytebuf for null transferIndex {}", currTransferIndex);
+      pendingByteBufMap.putIfAbsent(currTransferIndex, new LinkedList<>());
+      pendingByteBufMap.get(currTransferIndex).add(buf);
+    } else {
+      if (pendingByteBufMap.containsKey(currTransferIndex)) {
+        LOG.info("Flushing pending bytebuf for transferIndex {}", currTransferIndex);
+        for (final ByteBuf pendingByte : pendingByteBufMap.remove(currTransferIndex)) {
+          inputContext.onByteBuf(pendingByte);
         }
-
-        //LOG.info("Add body to input context {}", inputContext.getContextId().getTransferIndex());
-        inputContext.onByteBuf(buf);
       }
 
-      onDataFrameEnd();
+      //LOG.info("Add body to input context {}", inputContext.getContextId().getTransferIndex());
+      inputContext.onByteBuf(buf);
     }
+
+    onDataFrameEnd();
 
     return in.readableBytes() > 0;
   }
