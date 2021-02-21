92891ee24792604b266361637e69c32650a9ebd6
taegeonum <taegeonum@gmail.com>
2021-02-21 21:38:57 +0900

    state store synchroinzed

--- runtime/executor/src/main/java/org/apache/nemo/runtime/executor/HDFStateStore.java
+++ runtime/executor/src/main/java/org/apache/nemo/runtime/executor/HDFStateStore.java
@@ -10,6 +10,8 @@ import javax.inject.Inject;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.hadoop.conf.Configuration;
 import org.slf4j.Logger;
@@ -25,75 +27,97 @@ public final class HDFStateStore implements StateStore {
   public HDFStateStore() throws IOException {
   }
 
+  private final Map<String, Object> taskLock = new ConcurrentHashMap<>();
+
   @Override
   public InputStream getStateStream(String taskId) {
-    final Path path = new Path(HDFSUtils.STATE_PATH + "/" +  taskId);
-    try {
-      final FileSystem fileSystem = path.getFileSystem(conf);
-      return fileSystem.open(path);
-    } catch (IOException e) {
-      e.printStackTrace();
-      throw new RuntimeException(e);
+    taskLock.putIfAbsent(taskId, new Object());
+
+    synchronized (taskLock.get(taskId)) {
+      final Path path = new Path(HDFSUtils.STATE_PATH + "/" + taskId);
+      try {
+        final FileSystem fileSystem = path.getFileSystem(conf);
+        return fileSystem.open(path);
+      } catch (IOException e) {
+        e.printStackTrace();
+        throw new RuntimeException(e);
+      }
     }
   }
 
   @Override
   public byte[] getBytes(String taskId) {
-    final Path path = new Path(HDFSUtils.STATE_PATH + "/" +  taskId);
-    try {
-      final FileSystem fileSystem = path.getFileSystem(conf);
-      final long len = fileSystem.getFileStatus(path).getLen();
-      final byte[] bytes = new byte[(int)len];
-      fileSystem.open(path).readFully(bytes);
-      return bytes;
-    } catch (IOException e) {
-      e.printStackTrace();
-      throw new RuntimeException(e);
+    taskLock.putIfAbsent(taskId, new Object());
+
+    synchronized (taskLock.get(taskId)) {
+      final Path path = new Path(HDFSUtils.STATE_PATH + "/" + taskId);
+      try {
+        final FileSystem fileSystem = path.getFileSystem(conf);
+        final long len = fileSystem.getFileStatus(path).getLen();
+        final byte[] bytes = new byte[(int) len];
+        fileSystem.open(path).readFully(bytes);
+        return bytes;
+      } catch (IOException e) {
+        e.printStackTrace();
+        throw new RuntimeException(e);
+      }
     }
   }
 
   @Override
   public OutputStream getOutputStream(String taskId) {
-    final Path path = new Path(HDFSUtils.STATE_PATH + "/" +  taskId);
-    try {
-      final FileSystem fileSystem = path.getFileSystem(conf);
-      return fileSystem.create(path);
-    } catch (IOException e) {
-      e.printStackTrace();
-      throw new RuntimeException(e);
+    taskLock.putIfAbsent(taskId, new Object());
+
+    synchronized (taskLock.get(taskId)) {
+      final Path path = new Path(HDFSUtils.STATE_PATH + "/" + taskId);
+      try {
+        final FileSystem fileSystem = path.getFileSystem(conf);
+        return fileSystem.create(path);
+      } catch (IOException e) {
+        e.printStackTrace();
+        throw new RuntimeException(e);
+      }
     }
   }
 
   @Override
   public void put(String taskId, byte[] bytes) {
+    taskLock.putIfAbsent(taskId, new Object());
+
     LOG.info("Storing task state " + taskId + " into HDFS");
-    final Path path = new Path(HDFSUtils.STATE_PATH + "/" +  taskId);
-    try {
-      final FileSystem fileSystem = path.getFileSystem(conf);
+    synchronized (taskLock.get(taskId)) {
+      final Path path = new Path(HDFSUtils.STATE_PATH + "/" + taskId);
+      try {
+        final FileSystem fileSystem = path.getFileSystem(conf);
 
-      if (fileSystem.exists(path)) {
-        LOG.info("Task state " + taskId + " already exist.. remove and rewrite");
-        fileSystem.delete(path, true);
+        if (fileSystem.exists(path)) {
+          LOG.info("Task state " + taskId + " already exist.. remove and rewrite");
+          fileSystem.delete(path, true);
+        }
+
+        final FSDataOutputStream out = fileSystem.create(path);
+        out.write(bytes);
+        out.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+        throw new RuntimeException(e);
       }
-
-      final FSDataOutputStream out = fileSystem.create(path);
-      out.write(bytes);
-      out.close();
-    } catch (IOException e) {
-      e.printStackTrace();
-      throw new RuntimeException(e);
     }
   }
 
   @Override
   public boolean containsState(String taskId) {
-    final Path path = new Path(HDFSUtils.STATE_PATH + "/" +  taskId);
-    try {
-      final FileSystem fileSystem = path.getFileSystem(conf);
-      return fileSystem.exists(path);
-    } catch (IOException e) {
-      e.printStackTrace();
-      throw new RuntimeException(e);
+    taskLock.putIfAbsent(taskId, new Object());
+
+    synchronized (taskLock.get(taskId)) {
+      final Path path = new Path(HDFSUtils.STATE_PATH + "/" + taskId);
+      try {
+        final FileSystem fileSystem = path.getFileSystem(conf);
+        return fileSystem.exists(path);
+      } catch (IOException e) {
+        e.printStackTrace();
+        throw new RuntimeException(e);
+      }
     }
   }
 
