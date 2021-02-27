e439d3a0a2d7bfe953cad4fb4035b93ebf4e9a3d
taegeonum <taegeonum@gmail.com>
2021-02-27 20:08:20 +0900

    logging

--- offloading/common/src/main/java/org/apache/nemo/offloading/common/OffloadingExecutorControlEventCoder.java
+++ offloading/common/src/main/java/org/apache/nemo/offloading/common/OffloadingExecutorControlEventCoder.java
@@ -5,22 +5,27 @@ import io.netty.buffer.CompositeByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToMessageDecoder;
 import io.netty.handler.codec.MessageToMessageEncoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public final class OffloadingExecutorControlEventCoder {
+  private static final Logger LOG = LoggerFactory.getLogger(OffloadingExecutorControlEventCoder.class.getName());
 
   public static final class OffloadingEventEncoder extends MessageToMessageEncoder<OffloadingExecutorControlEvent> {
 
     @Override
     protected void encode(ChannelHandlerContext ctx, OffloadingExecutorControlEvent msg, List<Object> out) throws Exception {
-      final ByteBuf buf = ctx.alloc().buffer(1 + Integer.BYTES);
+      final ByteBuf buf = ctx.alloc().buffer();
       buf.writeByte(0);
       buf.writeInt(msg.getType().ordinal());
 
       if (msg.getByteBuf() != null) {
         buf.writeBytes(msg.getByteBuf());
       }
+
+      LOG.info("Encoding control event for type " + msg.getType().ordinal() + ", readable " + buf.readableBytes());
 
       out.add(buf);
     }
