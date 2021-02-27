65cc9298b740d0a06e0f35a0ced82bcf9430ad57
taegeonum <taegeonum@gmail.com>
2021-02-27 22:59:00 +0900

    fix

--- runtime/executor-common/src/main/java/org/apache/nemo/runtime/executor/common/DefaultTaskExecutorImpl.java
+++ runtime/executor-common/src/main/java/org/apache/nemo/runtime/executor/common/DefaultTaskExecutorImpl.java
@@ -636,9 +636,9 @@ public final class DefaultTaskExecutorImpl implements TaskExecutor {
     if (curr - flushBufferTime < 2000) {
       return 6000;
     } else if (curr - flushBufferTime < 4000) {
-      return 9000;
-    } else if (curr - flushBufferTime < 6000) {
       return 12000;
+    } else if (curr - flushBufferTime < 6000) {
+      return 15000;
     } else {
       return 100000;
     }
