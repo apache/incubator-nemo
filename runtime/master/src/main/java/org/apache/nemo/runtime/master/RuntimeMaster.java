0b4bdb9d0913c59c860091408605d113a88142b0
taegeonum <taegeonum@gmail.com>
2021-03-17 16:42:53 +0900

    fix thread stop

--- runtime/master/src/main/java/org/apache/nemo/runtime/master/RuntimeMaster.java
+++ runtime/master/src/main/java/org/apache/nemo/runtime/master/RuntimeMaster.java
@@ -751,18 +751,16 @@ public final class RuntimeMaster {
   private void handleControlMessage(final ControlMessage.Message message) {
     switch (message.getType()) {
       case StopTaskDone: {
-        requestContainerThread.execute(() -> {
-          final ControlMessage.StopTaskDoneMessage stopTaskDone = message.getStopTaskDoneMsg();
-          LOG.info("Receive stop task done message " + stopTaskDone.getTaskId() + ", " + stopTaskDone.getExecutorId());
-          final ExecutorRepresenter executorRepresenter =
-            executorRegistry.getExecutorRepresentor(stopTaskDone.getExecutorId());
-          executorRepresenter.onTaskExecutionStop(stopTaskDone.getTaskId());
-          final Task task = taskScheduledMap.removeTask(stopTaskDone.getTaskId());
-          LOG.info("Change task state to READY " + stopTaskDone.getTaskId());
-          planStateManager.onTaskStateChanged(stopTaskDone.getTaskId(), TaskState.State.READY);
-          pendingTaskCollectionPointer.addTask(task);
-          taskDispatcher.onNewPendingTaskCollectionAvailable();
-        });
+        final ControlMessage.StopTaskDoneMessage stopTaskDone = message.getStopTaskDoneMsg();
+        LOG.info("Receive stop task done message " + stopTaskDone.getTaskId() + ", " + stopTaskDone.getExecutorId());
+        final ExecutorRepresenter executorRepresenter =
+          executorRegistry.getExecutorRepresentor(stopTaskDone.getExecutorId());
+        executorRepresenter.onTaskExecutionStop(stopTaskDone.getTaskId());
+        final Task task = taskScheduledMap.removeTask(stopTaskDone.getTaskId());
+        LOG.info("Change task state to READY " + stopTaskDone.getTaskId());
+        planStateManager.onTaskStateChanged(stopTaskDone.getTaskId(), TaskState.State.READY);
+        pendingTaskCollectionPointer.addTask(task);
+        taskDispatcher.onNewPendingTaskCollectionAvailable();
         break;
       }
       case TaskStateChanged:
