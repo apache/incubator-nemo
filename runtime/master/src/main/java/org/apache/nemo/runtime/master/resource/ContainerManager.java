cb8b494d693667580a27dfc9f0975641be2e74a4
taegeonum <taegeonum@gmail.com>
2021-03-31 17:41:01 +0900

    rm g1gc

--- runtime/master/src/main/java/org/apache/nemo/runtime/master/resource/ContainerManager.java
+++ runtime/master/src/main/java/org/apache/nemo/runtime/master/resource/ContainerManager.java
@@ -174,10 +174,10 @@ public final class ContainerManager {
       .addOption("-XX:+PrintGCDetails")
       .addOption("-XX:+PrintGCTimeStamps")
       .addOption("-XX:+PrintGCDateStamps")
-      .addOption("-XX:+UseG1GC")
+      // .addOption("-XX:+UseG1GC")
       .addOption("-XX:InitialHeapSize=" + (resourceSpecification.getMemory() - 100) + "m")
-      .addOption("-XX:MaxHeapSize=" + (resourceSpecification.getMemory() - 100) + "m")
-      .addOption("-XX:MaxGCPauseMillis=500");
+      .addOption("-XX:MaxHeapSize=" + (resourceSpecification.getMemory() - 100) + "m");
+      //.addOption("-XX:MaxGCPauseMillis=500");
       //.addOption("-verbosegc");
 
     /*
